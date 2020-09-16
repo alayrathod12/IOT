a766b0b9fe8787791a9ac5dec23466ca8cd60e43
alayrathod12 <71247276+alayrathod12@users.noreply.github.com>
2020-09-16 16:53:43 +0530

    Telebotprac1

---
+++ Prac_1.py
@@ -0,0 +1,17 @@
+import telepot
+token = '966140475:AAFrB6R9uIjeLym4-4OMSgnf2HigQATPROU'
+TelegramBot = telepot.Bot(token)
+print (TelegramBot.getMe())
+
+
+https://api.telegram.org/bot966140475:AAFrB6R9uIjeLym4-4OMSgnf2HigQATPROU/getMe
+
+https://api.telegram.org/bot966140475:AAFrB6R9uIjeLym4-4OMSgnf2HigQATPROU/getUpdates
+
+Id: 1374937435
+
+https://api.telegram.org/bot966140475:AAFrB6R9uIjeLym4-4OMSgnf2HigQATPROU/
+
+https://api.telegram.org/bot.966140475:AAFrB6R9uIjeLym4-4OMSgnf2HigQATPROU/sendMessage?chat_id=1113753715
+
+https://api.telegram.org/bot.966140475:AAFrB6R9uIjeLym4-4OMSgnf2HigQATPROU/sendMessage?chat_id=1113753715&text=Hello Alay
